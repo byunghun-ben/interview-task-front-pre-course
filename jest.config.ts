02a3b97434d04e07cf24abba3f2f6d37ce9f3fbc
byunghun-ben <figma@kakao.com>
2024-10-09 21:43:02 +0900

    jest 설정   - typescript

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,10 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
+  preset: "ts-jest",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
 };
 
 export default config;
